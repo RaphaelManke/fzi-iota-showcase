64932b1aad36adac68175190abaca1106db681e8
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-03-03 17:30:48 +0100

    added project structure

---
+++ showcase-user-interface/src/custom_typings/vue-socket.io/index.d.ts
@@ -0,0 +1,21 @@
+declare module "vue-socket.io" {
+  import _Vue, {PluginFunction, PluginObject} from 'vue';
+  import {Store} from 'vuex';
+  import * as SocketIOClient from 'socket.io-client';
+
+  export default class VueSocketIO implements PluginObject<Options> {
+    constructor(options?: Options);
+
+    install: PluginFunction<any>;
+  }
+
+  export type Options = {
+    debug: boolean,
+    vuex: {
+      store: Store<any>,
+      actionPrefix: string,
+      mutationPrefix: string
+    }
+    connection: string | SocketIOClient.Socket
+  }
+}
