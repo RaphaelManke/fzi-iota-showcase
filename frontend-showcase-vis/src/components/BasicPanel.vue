03847f34ca540a422053a01d938ad3f3c8d83729
Christian Fries <undzn@student.kit.edu>
2019-03-11 15:32:43 +0100

    simple gui pre-version

---
+++ frontend-showcase-vis/src/components/BasicPanel.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="basicPanel">
+
+      <h1>Feld {{ location }}</h1>
+
+  </div>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'basicPanel',
+  props: ['location'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.basicPanel {
+  width: 100%; 
+  height: 0; 
+  padding-bottom: 100%;
+  background-color: green;
+  text-align: center;
+}
+
+h1 {
+  margin-top: 45%;
+}
+
+
+</style>
---
+++ frontend-showcase-vis/src/components/Vehicle.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div class="vehicle">
+
+      <img src="../assets/car.png">
+
+  </div>
+  
+</template>
+
+<script>
+
+
+export default {
+  name: 'vehicle',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.vehicle {
+    position: absolute;
+    text-align: center;
+    height: 8%;
+    width: 8%;
+}
+
+img {
+    height: 100%;
+}
+
+</style>
