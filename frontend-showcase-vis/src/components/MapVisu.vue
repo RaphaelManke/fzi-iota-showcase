cec47149101f21365bd8508da5eee81fc5ebf507
Christian Fries <undzn@student.kit.edu>
2019-03-19 13:01:45 +0100

    clean out code

--- frontend-showcase-vis/src/components/MapVisu.vue
+++ frontend-showcase-vis/src/components/MapVisu.vue
@@ -2,34 +2,23 @@
 
     <div id="mapVisu">
 
-        <div id="mapGrid">
-
-        <basicPanel v-for="entry in matrix" :location="entry"></basicPanel>
-
-        </div>
-
-        <vehicleSpace></vehicleSpace>
+        
 
     </div>
     
 </template>
 
 <script>
-
-import BasicPanel from './BasicPanel';
-import VehicleSpace from './VehicleSpace';
 import { eventBus } from './../events.ts';
 
 
 export default{
   data() {
       return {
-          matrix: ['1', '2', '3', '4', '5', '6', '7', '8'],
+          
       };
   },
   components: {
-      BasicPanel,
-      VehicleSpace,
   },
   methods: {},
   computed: {},
@@ -38,17 +27,6 @@ export default{
 
 <style scoped>
 
-#mapVisu {
-    position: relative;
-    border: 10px solid gray;
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.3), 0 12px 40px 0 rgba(0, 0, 0, 0.3);
-    border-radius: 10px;
-}
-
-#mapGrid {
-    display: grid;
-    grid-template-columns: auto auto auto auto;
-}
 
 </style>
 
