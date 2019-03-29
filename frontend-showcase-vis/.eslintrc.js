ec6592d70b6d7097d22e02ab678e2db42aeeadbc
Raphael <raphaz@gmx.de>
2019-03-29 12:24:55 +0100

    make vscode fix all problemshopefully

---
+++ frontend-showcase-vis/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+
+  parserOptions: {
+    sourceType: "module",
+    parser: "@typescript-eslint/parser"
+  },
+
+  env: {
+    browser: true,
+    node: true
+  },
+
+  extends: ["prettier", "prettier/standard", "plugin:vue/recommended"],
+  plugins: ["vue", "prettier"],
+
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+
+  extends: [
+    "prettier",
+    "prettier/standard",
+    "plugin:vue/recommended",
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "@vue/typescript"
+  ]
+};
