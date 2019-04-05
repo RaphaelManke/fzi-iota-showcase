35179e46b4d1cc0ae85aa7eb2d4e6b3bdeeb87f6
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-05 13:28:40 +0200

    added create payment seed method

--- vehicle-client/src/seeds.ts
+++ vehicle-client/src/seeds.ts
@@ -1,11 +1,12 @@
-import {explode} from './explode';
-import {trits, trytes, fromValue} from '@iota/converter';
-import {add} from '@iota/signing';
+import { explode } from './explode';
+import { trits, trytes, fromValue } from '@iota/converter';
+import { add } from '@iota/signing';
 import Kerl from '@iota/kerl';
-import {BigNumber} from 'bignumber.js';
+import { BigNumber } from 'bignumber.js';
 
 const MAX_VALUE = new BigNumber(value(trits('M'.repeat(81))));
 const DEFAULT_MAM_TREE_SIZE = 1;
+
 export const TRIP_CHANNEL_CAPACITY = 2;
 export let checkedMetaInfoCapacity = DEFAULT_MAM_TREE_SIZE * 500;
 export let checkedReservationCapacity = DEFAULT_MAM_TREE_SIZE * 1000;
@@ -15,19 +16,33 @@ export function getTripSeed(seed: string, index: number) {
 }
 
 export function getReservationSeed(seed: string, tripIndex: number) {
-  return trytes(hashUntilCapacity(explode(getTripSeedTrits(seed, tripIndex)), checkedReservationCapacity));
+  return trytes(
+    hashUntilCapacity(
+      explode(getTripSeedTrits(seed, tripIndex)),
+      checkedReservationCapacity,
+    ),
+  );
 }
 
 export function getMetaInfoSeed(seed: string) {
-  return trytes(hashUntilCapacity(explodeWithIndex(seed, 0), checkedMetaInfoCapacity));
+  return trytes(
+    hashUntilCapacity(explodeWithIndex(seed, 0), checkedMetaInfoCapacity),
+  );
 }
 
 export function getMasterSeed(seed: string, capacity: number) {
   return trytes(hashUntilCapacity(trits(seed), capacity));
 }
 
+export function getPaymentSeed(seed: string) {
+  return explodeWithIndex(seed, -1);
+}
+
 function getTripSeedTrits(seed: string, index: number) {
-  return hashUntilCapacity(explodeWithIndex(seed, index), TRIP_CHANNEL_CAPACITY);
+  return hashUntilCapacity(
+    explodeWithIndex(seed, index),
+    TRIP_CHANNEL_CAPACITY,
+  );
 }
 
 function explodeWithIndex(seed: string, index: number) {
@@ -49,7 +64,7 @@ function value(input: Int8Array) {
   let returnValue = new BigNumber(0);
 
   for (let i = input.length; i-- > 0; ) {
-      returnValue = returnValue.times(3).plus(input[i]);
+    returnValue = returnValue.times(3).plus(input[i]);
   }
 
   return returnValue;
