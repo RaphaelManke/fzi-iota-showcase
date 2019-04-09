3ee878ba6d66052024701428b17911e533d56057
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-09 08:36:48 +0200

    fix bug in router

--- vehicle-mock/src/router.ts
+++ vehicle-mock/src/router.ts
@@ -4,14 +4,24 @@ import { Trytes } from '@iota/core/typings/types';
 export class Router {
   private conntectedTo = new Map<Trytes, Set<Connection>>();
 
-  constructor(connections: Connection[], private readonly cached: RouteResult[] = []) {
-    const stops = new Set([...connections.map((c) => c.from), ...connections.map((c) => c.to)]);
+  constructor(
+    connections: Connection[],
+    private readonly cached: RouteResult[] = [],
+  ) {
+    const stops = new Set([
+      ...connections.map((c) => c.from),
+      ...connections.map((c) => c.to),
+    ]);
     stops.forEach((stop) => {
       const to = connections.filter((c) => c.from === stop);
       this.add(stop, to);
       // reverse direction
-      const reversed: Connection[] = to.map((c) =>
-        ({from: c.to, to: c.from, path: Array.from(c.path).reverse(), type: c.type}));
+      const reversed: Connection[] = to.map((c) => ({
+        from: c.to,
+        to: c.from,
+        path: Array.from(c.path).reverse(),
+        type: c.type,
+      }));
       reversed.forEach((c) => this.add(c.from, [c]));
     });
   }
@@ -21,8 +31,12 @@ export class Router {
     if (cached.length > 0) {
       return cached;
     }
-    const visited = new Set<Trytes>();
-    const innerGetPath = (s: Trytes, d: Trytes, index: number): Route[] => {
+    const innerGetPath = (
+      s: Trytes,
+      d: Trytes,
+      index: number,
+      visited: Set<Trytes>,
+    ): Route[] => {
       const cons = this.conntectedTo.get(s);
       visited.add(s);
       // no connections from s
@@ -33,31 +47,57 @@ export class Router {
       const [con] = arr.filter((c) => c.to === d);
       // direct connection from s to d
       if (con) {
-        return [{path: con.path, stops: [{id: s, index}, {id: d, index: index + con.path.length - 1 }]}];
+        return [
+          {
+            path: con.path,
+            stops: [
+              { id: s, index },
+              { id: d, index: index + con.path.length - 1 },
+            ],
+          },
+        ];
       }
 
-      const innerRoutes: Route[] = arr.filter((c) => !visited.has(c.to)).map((c) => {
-        // paths from stop connected to s to dest
-        const paths = innerGetPath(c.to, dest, index + c.path.length - 1);
-        return paths.map((path) => {
-          const front = equals(c.path[c.path.length - 1], path.path[0]) ?
-            c.path.slice(0, c.path.length - 1) : c.path;
-          return {stops: [{id: s, index}, ...path.stops], path: [...front, ...path.path]};
-        });
-      })
-      .reduce((acc, v) => {v.forEach((p) => acc.push(p)); return acc; }, new Array<Route>())
-      .sort((a, b) => a.path.length - b.path.length);
+      const innerRoutes: Route[] = arr
+        .filter((c) => !visited.has(c.to))
+        .map((c) => {
+          // paths from stop connected to s to dest
+          const paths = innerGetPath(
+            c.to,
+            dest,
+            index + c.path.length - 1,
+            new Set<Trytes>(visited),
+          );
+          return paths.map((path) => {
+            const front = equals(c.path[c.path.length - 1], path.path[0])
+              ? c.path.slice(0, c.path.length - 1)
+              : c.path;
+            return {
+              stops: [{ id: s, index }, ...path.stops],
+              path: [...front, ...path.path],
+            };
+          });
+        })
+        .reduce((acc, v) => {
+          v.forEach((p) => acc.push(p));
+          return acc;
+        }, new Array<Route>())
+        .sort((a, b) => a.path.length - b.path.length);
 
       return innerRoutes;
     };
 
-    const routes = innerGetPath(start, dest, 0).sort((a, b) => a.path.length - b.path.length);
-    this.cached.push({start, dest, type, routes});
+    const routes = innerGetPath(start, dest, 0, new Set<Trytes>()).sort(
+      (a, b) => a.path.length - b.path.length,
+    );
+    this.cached.push({ start, dest, type, routes });
     return routes;
   }
 
   public getCached(start: Trytes, dest: Trytes, type: Type) {
-    const result = this.cached.find((r) => r.start === start && r.dest === dest && r.type === type);
+    const result = this.cached.find(
+      (r) => r.start === start && r.dest === dest && r.type === type,
+    );
     return result ? result.routes : [];
   }
 
@@ -84,7 +124,7 @@ export interface RouteResult {
 
 export interface Route {
   path: Position[];
-  stops: Array<{id: Trytes, index: number}>;
+  stops: Array<{ id: Trytes; index: number }>;
 }
 export interface Connection {
   from: Trytes;
