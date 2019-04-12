26986437efec8bc82d61b8988309ff92435cec14
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-12 16:06:23 +0200

    add nonce verifying

--- user-client/src/tripHandler.ts
+++ user-client/src/tripHandler.ts
@@ -1,7 +1,10 @@
 import {
   PaymentChannel,
   VehicleAuthenticationMessage,
+  CheckInMessage,
 } from 'fzi-iota-showcase-client';
+import { trits, trytes } from '@iota/converter';
+import Kerl from '@iota/kerl';
 import { Trytes } from '@iota/core/typings/types';
 
 export class TripHandler {
@@ -10,16 +13,35 @@ export class TripHandler {
 
   constructor(
     private destination: Trytes,
+    private checkInMessage: CheckInMessage,
+    private nonce: Trytes,
     private paymentChannel: PaymentChannel<any, any, any>,
     private sender: Sender,
   ) {}
 
   public onVehicleAuthentication(message: VehicleAuthenticationMessage) {
-    message.sendAuth;
+    if (hash(message.nonce) === this.checkInMessage.hashedNonce) {
+      if (message.sendAuth) {
+        this.sender.userAuthentication(this.nonce);
+      }
+    } else {
+    }
   }
 }
 
-export interface Sender {}
+function hash(value: Trytes) {
+  const kerl = new Kerl();
+  kerl.initialize();
+  const input = trits(value);
+  kerl.absorb(input, 0, input.length);
+  const result = new Int8Array(243);
+  kerl.squeeze(result, 0, input.length);
+  return trytes(result);
+}
+
+export interface Sender {
+  userAuthentication(nonce: Trytes): void;
+}
 
 export enum State {
   AUTHENTICATION_REQUESTED,
