ec112f2635d46810a9db3b5ede943af72599e6a8
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-12 23:27:38 +0200

    add boarding handler to exports

--- vehicle-client/src/index.ts
+++ vehicle-client/src/index.ts
@@ -3,3 +3,4 @@ export * from './checkOutPublisher';
 export * from './reservationPublisher';
 export * from './vehiclePublisher';
 export { getPaymentSeed } from './seeds';
+export * from './boardingHandler';
