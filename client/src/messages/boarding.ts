056f08415031a30920b1cb8511ac2930cf70e694
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-12 15:16:25 +0200

    boarding data model

--- client/src/messages/boarding.ts
+++ client/src/messages/boarding.ts
@@ -1,5 +1,55 @@
 import { Trytes, Hash } from '@iota/core/typings/types';
 
+export interface PaymentChannel<DIGEST, MULTISIG_ADDRESS, BUNDLE> {
+  state: PaymentChannelState;
+
+  open(
+    settlementAddress: Hash,
+    userIndex: number,
+    seed: number,
+    signersCount: number,
+    settlement: Hash,
+    digests: DIGEST[], // TODO
+    depth: number,
+    security: number,
+  ): void;
+
+  updateDeposit(deposits: number[]): void;
+
+  prepareChannel(allDigests: DIGEST[], settlementAddresses: Hash[]): void;
+
+  applyTransaction(signedBundles: BUNDLE[]): void;
+
+  attachCurrentBundle(): Hash;
+
+  buildNewBranch(
+    allDigests: DIGEST[],
+    multisigAddress: MULTISIG_ADDRESS,
+  ): MULTISIG_ADDRESS[];
+
+  createTransaction(
+    amount: number,
+    address: Hash,
+    onCreateNewBranch: (multisig: MULTISIG_ADDRESS, generate: number) => void,
+  ): { bundles: BUNDLE[]; signedBundles: BUNDLE[] };
+
+  createCloseTransaction(): { bundles: BUNDLE[]; signedBundles: BUNDLE[] };
+
+  signTransaction(bundles: BUNDLE[], signedBundles: BUNDLE[]): any[];
+
+  createDigests(amount: number): DIGEST[];
+}
+
+export enum PaymentChannelState {
+  CREATED,
+  GENERATED_DIGESTS,
+  WAIT_FOR_DEPOSIT,
+  READY,
+  SIGNED_TRANSACTION,
+  PREPARE_NEW_BRANCH,
+  CLOSED,
+}
+
 export interface VehicleAuthenticationMessage {
   nonce: Trytes;
   sendAuth: boolean;
@@ -14,10 +64,33 @@ export interface PriceMessage {
   price: number;
 }
 
-export interface CreatePaymentChannelMessage {}
+export interface OpenPaymentChannelMessage {
+  userIndex: number;
+  settlement: Hash;
+  digests: any[]; // TODO
+  depth: number;
+  security: number;
+}
+
+export interface DepositSentMessage {
+  depositTransaction: Hash;
+  amount: number;
+}
 
 export interface TransactionCreatedMessage {}
 
 export interface TransactionSignedMessage {}
 
-export interface CreateNewBranchMessage {}
+export interface CreatedNewBranchMessage {}
+
+export interface CreditsLeftMessage {
+  amount: number;
+  distance: number;
+  millis: number;
+}
+
+export interface CreditsExaustedMessage {
+  minimumAmount: number;
+}
+
+export interface ClosePaymentChannelMessage {}
