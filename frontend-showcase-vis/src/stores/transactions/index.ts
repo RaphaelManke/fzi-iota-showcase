227a58b32711c4968c3c99ec015b7886724f5e56
Christian Fries <undzn@student.kit.edu>
2019-04-13 17:38:27 +0200

    shoe checkIn as a transaction?

--- frontend-showcase-vis/src/stores/transactions/index.ts
+++ frontend-showcase-vis/src/stores/transactions/index.ts
@@ -7,6 +7,15 @@ export const transactions: Module<TransactionStore, RootState> = {
     transactions: []
   },
   mutations: {
+    SOCKET_CheckIn(state: any, data: any) {
+      if (state.transactions.length > 50) {
+        state.transactions.shift();
+      }
+      state.transactions = [
+        ...state.transactions,
+        { from: data.vehicleId, to: "Tangle", amount: 0 }
+      ];
+    },
     SOCKET_TransactionIssued(state: any, data: any) {
       if (state.transactions.length > 50) {
         state.transactions.shift();
