d6500d1457ff2327a4a203ba65baea08ecd9cecc
Lamberti, Robin <robin.lamberti@iosb.fraunhofer.de>
2019-04-18 19:30:06 +0200

    force mam ts to use powsrv without api_key

--- vehicle-client/src/vehiclePublisher.ts
+++ vehicle-client/src/vehiclePublisher.ts
@@ -83,6 +83,9 @@ async function publishMetaInfo(
 function createMetaInfoWriter(provider: string, seed: string) {
   const metaInfoSeed = getMetaInfoSeed(seed);
   const infoChannel = new MamWriter(provider, metaInfoSeed, MAM_MODE.PUBLIC);
-  infoChannel.EnablePowSrv(true);
+  Object.assign(infoChannel, {
+    // dirty hack. mam.ts supports powsrv only with api_key
+    attachFunction: createAttachToTangle(),
+  });
   return infoChannel;
 }
